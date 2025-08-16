626ffbd6774f4d41b2319820cc5b864c0276b465
Ted1166 <ogolated00@gmail.com>
2025-08-16 04:47:56 +0300

    index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,30 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/" element={<NotFound />} />
+        <Route path="*import React, { Component } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+class App extends Component {
+  render() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    );
+  }
+}
+
+export default App;" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>
